2b44c6df0b4a5f7da5987eadbd6126bba4b7ea13
taaaf11 <taafuuu@gmail.com>
2026-09-20 19:11:19 +0500

    Update content, styles and scripts

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 let button = document.getElementById('click-me-btn-btn');
 console.log('lss')
 button.addEventListener('click', async () => {
-  alert("Button was clicked!")
-  
-  let response = await fetch("https://web-theory-assignment-1-chi.vercel.app/api/click");
-  let json = await response.json();
-
-  console.log(json.visits)
+    alert("Button was clicked!")
 });
